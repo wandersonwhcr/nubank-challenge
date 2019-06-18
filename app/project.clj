72f26bde2728b8afcbafae850e43d7705590d3d5
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-17 21:43:19 -0300

    Melhora descrição

--- app/project.clj
+++ app/project.clj
@@ -1,6 +1,5 @@
-(defproject balance "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject balance "1.0.0-alpha"
+  :description "Balance with Clojure"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
