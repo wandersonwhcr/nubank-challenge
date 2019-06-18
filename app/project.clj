d23a9b853f51cde87ca962fad36e6b290ba64565
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-17 21:49:10 -0300

    Inclui como dependência o manipulador JSON

--- app/project.clj
+++ app/project.clj
@@ -3,7 +3,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler balance.handler/app :open-browser? false}
   :profiles
