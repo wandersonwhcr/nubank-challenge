d8c743e723d56cef74b064fdd249cfbda0361a05
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-18 22:13:39 -0300

    Salva informações enviadas

--- app/src/balance/handler.clj
+++ app/src/balance/handler.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.json :as json]
-            [ring.util.response :refer :all]))
+            [ring.util.response :refer [response]]))
 
 (def users {
   "abc123" {:id "abc123" :name "John Doe"}
@@ -12,13 +12,18 @@
 
 (defn get-user [id] (get users id))
 
+(defn save-user [data] (do
+  (def users (assoc users "def789" data))
+  nil))
+
 (defroutes app-routes
   (GET "/" [] (response {:message "Hello World"}))
   (GET "/v1/users" [] (response (get-users)))
   (GET "/v1/users/:id" [id] (response (get-user id)))
+  (POST "/v1/users" {:keys [body]} (response (save-user body)))
   (route/not-found (response {:message "Not Found"})))
 
 (def app (->
   app-routes
-  json/wrap-json-body
+  (json/wrap-json-body {:keywords? true})
   json/wrap-json-response))
