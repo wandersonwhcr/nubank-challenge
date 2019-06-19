4140c756dfabe3d74bac6dc65ee40d48dd9edb9d
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-18 21:23:38 -0300

    Apresenta uma listagem simples de usuários

--- app/src/balance/handler.clj
+++ app/src/balance/handler.clj
@@ -4,8 +4,13 @@
             [ring.middleware.json :as json]
             [ring.util.response :refer :all]))
 
+(defn get-users [] [
+  {:id "abc123" :name "John Doe"}
+  {:id "xyz456" :name "João da Silva"}])
+
 (defroutes app-routes
   (GET "/" [] (response {:message "Hello World"}))
+  (GET "/v1/users" [] (response (get-users)))
   (route/not-found (response {:message "Not Found"})))
 
 (def app (->
