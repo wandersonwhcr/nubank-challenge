e9fc8601b675ce9f6b6185884af41b9af8dc4177
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-18 21:05:02 -0300

    Efetua limpeza em dependências

--- app/src/balance/handler.clj
+++ app/src/balance/handler.clj
@@ -1,11 +1,9 @@
 (ns balance.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (-> app-routes))
