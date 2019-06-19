9cd5fcd9679b5932b94573694c9c2514f176ba8d
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-19 00:25:38 -0300

    Inclui tratamento de usuário desconhecido

--- app/src/balance/service/users.clj
+++ app/src/balance/service/users.clj
@@ -7,15 +7,21 @@
 ;;; Show Users
 (defn fetch [] (vals users))
 
+;;; Check a User by Identifier
+(defn check [id]
+  (when (not (contains? users id))
+    (throw (RuntimeException. (str "Unknown User " id)))))
+
 ;;; Show a User by Identifier
-(defn by [id] (get users id))
+(defn by [id] (do
+  (check id)
+  (get users id)))
 
 ;;; Save a User into Data HashMap
 (defn save [data] (do
-  (def users (assoc users (get data :id) data))
-  nil))
+  (def users (assoc users (get data :id) data)) nil))
 
 ;;; Delete a User by Identifier
 (defn delete [id] (do
-  (def users (dissoc users id))
-  nil))
+  (check id)
+  (def users (dissoc users id)) nil))
