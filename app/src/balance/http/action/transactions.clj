8ed85f289295a1cf5b25c02f60c91762b088ae71
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-20 16:41:28 -0300

    Adiciona tratamento de erros

--- app/src/balance/http/action/transactions.clj
+++ app/src/balance/http/action/transactions.clj
@@ -7,22 +7,49 @@
     [balance.service.users :as users-service]
     [balance.service.transactions :as transactions-service]))
 
-(defn fetch [user-id] (-> user-id
-  (users-service/find)
-  (transactions-service/fetchByUser)
-  (response)))
+(defn fetch [user-id]
+  (try
+    (-> user-id
+      (users-service/find)
+      (transactions-service/fetchByUser)
+      (response))
+    (catch Exception e
+      (case (:type (ex-data e))
+        :user-not-found (not-found (ex-data e))
+        (internal-error)))))
 
-(defn save [user-id data] (-> user-id
-  (users-service/find)
-  (transactions-service/saveByUser (set-uuid data))
-  (saved)))
+(defn save [user-id data]
+  (try
+    (-> user-id
+      (users-service/find)
+      (transactions-service/saveByUser (set-uuid data))
+      (saved))
+    (catch Exception e
+      (case (:type (ex-data e))
+        :user-not-found (not-found (ex-data e))
+        :transaction-invalid-data (unprocessable-entity (ex-data e))
+        (internal-error)))))
 
-(defn find [user-id transaction-id] (-> user-id
-  (users-service/find)
-  (transactions-service/findByUser transaction-id)
-  (response)))
+(defn find [user-id transaction-id]
+  (try
+    (-> user-id
+      (users-service/find)
+      (transactions-service/findByUser transaction-id)
+      (response))
+    (catch Exception e
+      (case (:type (ex-data e))
+        :user-not-found (not-found (ex-data e))
+        :transaction-not-found (not-found (ex-data e))
+        (internal-error)))))
 
-(defn delete [user-id transaction-id] (-> user-id
-  (users-service/find)
-  (transactions-service/deleteByUser transaction-id)
-  (no-content)))
+(defn delete [user-id transaction-id]
+  (try
+    (-> user-id
+      (users-service/find)
+      (transactions-service/deleteByUser transaction-id)
+      (no-content))
+    (catch Exception e
+      (case (:type (ex-data e))
+        :user-not-found (not-found (ex-data e))
+        :transaction-not-found (not-found (ex-data e))
+        (internal-error)))))
