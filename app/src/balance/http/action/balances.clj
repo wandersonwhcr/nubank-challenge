4922a37d366c45a3fa93ce271c04fe87aa964228
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-20 17:16:29 -0300

    Adiciona tratamento de erros

--- app/src/balance/http/action/balances.clj
+++ app/src/balance/http/action/balances.clj
@@ -1,5 +1,10 @@
 (ns balance.http.action.balances
   (:refer-clojure :exclude [find])
-  (:require [ring.util.response :refer :all]))
+  (:require
+    [ring.util.response :refer :all]
+    [balance.http.response :refer :all]))
 
-(defn find [request] (response nil))
+(defn find [request]
+  (try
+    (response nil)
+    (catch Exception e (internal-error))))
