fb1f0b617c958d9e9a6e2000727091985235bf52
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-20 15:03:03 -0300

    Salva transações do usuário

--- app/src/balance/service/transactions.clj
+++ app/src/balance/service/transactions.clj
@@ -5,10 +5,18 @@
 ;;; Transactions Bucket
 (def ^:private transactions (atom {}))
 
-;;; Show Transactions
-(defn fetchByUser [user]
-  (filter (fn [transaction] (= (:id user) (:userId transaction))) (vals @transactions)))
+;;; Fetch Transactions by User
+(defn fetchByUser [user] (->> (vals @transactions)
+  ; Only Transactions for User
+  (filter (fn [transaction] (= (:id user) (:userId transaction))))
+  ; Remover User Identifier from Element
+  (map (fn [transaction] (dissoc transaction :userId)))))
 
 ;;; Save Transactions
-(defn saveByUser [user data]
-  (swap! transactions assoc (:id data) (merge data {:userId (:id user)})))
+(defn saveByUser [user data] (->> data
+  ; Configure User Identifier
+  (merge {:userId (:id user)})
+  ; Store Transaction
+  (swap! transactions assoc (:id data)))
+  ; Saved Data
+  (identity data));
