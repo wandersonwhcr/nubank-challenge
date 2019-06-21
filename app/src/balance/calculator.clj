e0879635a16e4f6ce931e49aa8375959ee8d23cb
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-20 21:12:53 -0300

    Separa método para converter transação

--- app/src/balance/calculator.clj
+++ app/src/balance/calculator.clj
@@ -1,11 +1,14 @@
 ;;;; Balance Calculator
 (ns balance.calculator)
 
+;;; Coerce Transaction to Value
+(defn to-decimal [transaction] (if (= (:type transaction) "in") (:value transaction) (- (:value transaction))))
+
 ;;; Calculate Balance by Transactions
 (defn calculate [transactions]
   (->> transactions
     ;; Transform: "in = value positive" | "out = value negative"
-    (map (fn [transaction] (if (= (:type transaction) "in") (:value transaction) (- (:value transaction)))))
+    (map to-decimal)
     ;; Transform: Values to BigDecimal
     (map (fn [value] (bigdec value)))
     ;; Summation
