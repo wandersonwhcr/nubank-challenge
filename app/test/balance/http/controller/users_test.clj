df7c3b5973cb18c83759bf8f59effd5c7b310fd9
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-22 19:57:49 -0300

    Salva e consulta usuários

--- app/test/balance/http/controller/users_test.clj
+++ app/test/balance/http/controller/users_test.clj
@@ -6,7 +6,7 @@
             [balance.service.users :as users-service]))
 
 (deftest test-users
-  (testing "users controller at fetch action"
+  (testing "fetch"
     (let [users-bucket (atom {})]
       (users-service/set-bucket users-bucket)
       (let [response (app (mock/request :get "/v1/users"))]
@@ -15,10 +15,23 @@
         (let [content (json/read-str (:body response))]
           (is (vector? content))))))
 
-  (testing "users controller at save action"
+  (testing "save"
     (let [users-bucket (atom {})]
       (users-service/set-bucket users-bucket)
       (let [response (app (-> (mock/request :post "/v1/users") (mock/json-body {:name "John Doe"})))]
         (is (= 201 (:status response)))
         (is (nil? (:body response)))
-        (is (string? (get-in response [:headers "Location"])))))))
+        (is (string? (get-in response [:headers "Location"]))))))
+
+  (testing "save and fetch"
+    (let [users-bucket (atom {})]
+      (users-service/set-bucket users-bucket)
+      (app (-> (mock/request :post "/v1/users") (mock/json-body {:name "John Doe"})))
+      (let [response (app (-> (mock/request :get "/v1/users")))]
+        (is (= 200 (:status response)))
+        (is (string? (:body response)))
+        (let [content (json/read-str (:body response))]
+          (is (vector? content))
+          (is (= 1 (count content)))
+          (is (contains? (get content 0) "id"))
+          (is (= "John Doe" (get-in content [0 "name"]))))))))
