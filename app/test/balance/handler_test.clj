ca219bf561c3232a7eca43cf095f213729e7ee58
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-22 18:28:14 -0300

    Inicializa controladoras

--- app/test/balance/handler_test.clj
+++ app/test/balance/handler_test.clj
@@ -4,11 +4,6 @@
             [balance.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= "" (:body response)))))
-
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404))
---
+++ app/test/balance/http/controller/home_test.clj
@@ -0,0 +1,9 @@
+(ns balance.http.controller.home-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [balance.handler :refer :all]))
+
+(deftest test-home
+  (testing "home controller at index action"
+    (let [response (app (mock/request :get "/"))]
+      (is (= 200 (:status response))))))
