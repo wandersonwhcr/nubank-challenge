7347577386ae73edb223cdaf771eac83c727b84c
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-22 00:55:11 -0300

    Padroniza captura de atributos

--- app/test/balance/http/response_test.clj
+++ app/test/balance/http/response_test.clj
@@ -5,22 +5,22 @@
 (deftest test-response
   (testing "no-content response"
     (let [response (no-content)]
-      (is (= 204 (response :status)))
+      (is (= 204 (:status response)))
       (is (contains? response :headers))
       (is (contains? response :body))))
   (testing "unprocessable-entity response without body"
     (let [response (unprocessable-entity)]
-      (is (= 422 (response :status)))
+      (is (= 422 (:status response)))
       (is (contains? response :headers))
       (is (contains? response :body))))
   (testing "unprocessable-entity response with body"
     (let [response (unprocessable-entity {:message "Something Happened"})]
-      (is (= 422 (response :status)))
+      (is (= 422 (:status response)))
       (is (contains? response :headers))
       (is (contains? response :body))
       (is (= "Something Happened" (get-in response [:body :message])))))
   (testing "internal-error response"
     (let [response (internal-error)]
-      (is (= 500 (response :status)))
+      (is (= 500 (:status response)))
       (is (contains? response :headers))
       (is (contains? response :body)))))
--- app/test/balance/util_test.clj
+++ app/test/balance/util_test.clj
@@ -13,11 +13,11 @@
   (testing "set"
     (let [element (set-uuid {})]
       (is (contains? element :id))
-      (is (string? (element :id)))
-      (is (re-matches #"^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$" (element :id)))))
+      (is (string? (:id element)))
+      (is (re-matches #"^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$" (:id element)))))
   (testing "random uuid"
     (let [elementA (set-uuid {}) elementB (set-uuid {})]
-      (is (not (= (elementA :id) (elementB :id))))))
+      (is (not (= (:id elementA) (:id elementB))))))
   (testing "assoc"
     (let [element (set-uuid {:name "John Doe"})]
       (is (contains? element :name)))))
