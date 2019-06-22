2294a26f555558ded385e0e7d95067a3e5807c5f
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-21 23:43:35 -0300

    Adiciona resposta sem conteúdo

--- app/src/balance/http/response.clj
+++ app/src/balance/http/response.clj
@@ -1,2 +1,6 @@
 (ns balance.http.response
   "Balance HTTP Response")
+
+(defn no-content
+  "HTTP 204 No Content"
+  [] nil)
--- app/test/balance/http/response_test.clj
+++ app/test/balance/http/response_test.clj
@@ -1,7 +1,8 @@
 (ns balance.http.response-test
   (:require [clojure.test :refer :all]
-            [balance.http.response]))
+            [balance.http.response :refer :all]))
 
 (deftest test-response
-  (testing "dumb"
-    (is (= true))))
+  (testing "no-content response"
+    (let [response (no-content)]
+      (is (nil? response)))))
