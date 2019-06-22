b7491b5bff39dd8a711fdc2ac87093e4e348a8af
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-21 23:20:58 -0300

    Gera UUIDs diferentes

--- app/test/balance/util_test.clj
+++ app/test/balance/util_test.clj
@@ -3,8 +3,11 @@
             [balance.util :refer :all]))
 
 (deftest test-util
-  (testing "set-uuid"
+  (testing "set"
     (let [element (set-uuid {})]
       (is (contains? element :id))
       (is (string? (:id element)))
-      (is (re-matches #"^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$" (:id element))))))
+      (is (re-matches #"^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$" (:id element)))))
+  (testing "random uuid"
+    (let [elementA (set-uuid {}) elementB (set-uuid {})]
+      (is (not (= (:id elementA) (:id elementB)))))))
