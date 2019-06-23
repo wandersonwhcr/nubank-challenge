f79ae5bfb908ff82efd4fd66398dcdd98b663cbb
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-23 17:00:10 -0300

    Adiciona outro caso de teste com erro

--- app/test/balance/balance/calculator_test.clj
+++ app/test/balance/balance/calculator_test.clj
@@ -19,6 +19,12 @@
     (let [transactions [(->Transaction (uuid) (uuid) "IN" 0.1) (->Transaction (uuid) (uuid) "IN" 0.2) (->Transaction (uuid) (uuid) "OUT" 0.3)]]
       (is (= 0.0 (calculate transactions)))))
 
+  (testing "floating point arithmetic problem"
+    (let [transactions [(->Transaction (uuid) (uuid) "IN" 1.99)
+                        (->Transaction (uuid) (uuid) "IN" 3.01)
+                        (->Transaction (uuid) (uuid) "OUT" 1.49)]]
+      (is (= 3.51 (calculate transactions)))))
+
   (testing "validate"
     (let [balance [(->Transaction (uuid) (uuid) "IN" 1.0)
                    (->Transaction (uuid) (uuid) "IN" 2.0)]]
