89bb1987c67da98ad1d5692fe7b412d6811f622a
Wanderson Henrique Camargo Rosa <wandersonwhcr@gmail.com>
2019-06-22 22:13:56 -0300

    Remove métodos não utilizados

--- app/src/balance/service/users.clj
+++ app/src/balance/service/users.clj
@@ -1,15 +1,10 @@
 (ns balance.service.users
   "Balance Users Service Layer"
-  (:refer-clojure :exclude [find])
-  (:require [json-schema.core :as json]))
+  (:refer-clojure :exclude [find]))
 
 (def ^:private ^:no-doc bucket
   "User Bucket"
   nil)
-
-(def ^:private schema
-  "User Schema Validation"
-  (slurp "src/balance/schema/users.json"))
 
 (defn set-bucket
   "Configures Current User Bucket"
@@ -23,22 +18,10 @@
   "Fetches Users"
   [] (if (empty? @bucket) [] (vals @bucket)))
 
-(defn validate
-  "Validates User"
-  [user]
-  (try
-    (json/validate schema user)
-    (catch Exception e (->>
-      (ex-data e)
-      (merge {:type :user-invalid-data})
-      (ex-info "Invalid Data")
-      (throw)))))
-
 (defn save
   "Saves User"
   [user]
   (do
-    (validate user)
     (swap! bucket assoc (:id user) user)
     (identity user)))
 
